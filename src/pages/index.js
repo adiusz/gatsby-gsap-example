1adbf24c03522e501e690b43131989ea54e2d705
Johannes Schwegler <johannes.schwegler@outlook.de>
2020-08-20 14:20:35 +0200

    Duration set to 0

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,10 +27,8 @@ const HeroFlexWrapper = styled.div`
   svg {
       width: 25vw;
       height: auto;
-      
-      g {
-        visibility: hidden;
-      }
+      visibility: hidden;
+   
     }
   
 `
@@ -42,7 +40,7 @@ const IndexPage = () => {
   useEffect(() => {
 
     const [svgElements] = svgWrapper.current.children;
-
+    const svg = document.getElementById("hero_graph")
     const AllG = svgElements.querySelectorAll("#hero_graph g")
     const HtmlTag = svgElements.querySelector('#html-tag');
     const Laptop = svgElements.querySelector('#laptop');
@@ -54,7 +52,7 @@ const IndexPage = () => {
 
     gsap.set([HtmlTag, Laptop, MobilePhone, CurlyBraces, Protect, Gear], {autoAlpha: 0, ease: 'power3.inOut'});
 
-    gsap.to(AllG, {duration: 2, css: {visibility: 'visible'}});
+    gsap.to(svg, {duration: 0, css: {visibility: 'visible'}});
 
     gsap.fromTo(CurlyBraces, {x: '-=100', autoAlpha: 0}, {delay: .8, duration: 1, x: '+=100', autoAlpha: 1});
     gsap.fromTo(Protect, {y: '-=100', autoAlpha: 0}, {delay: .3, duration: 1, y: '+=100', autoAlpha: 1});
